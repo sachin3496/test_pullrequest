1dc8b762af9c527ca229037021060f268c6ac4f6
sachin3496 <45626100+sachin3496@users.noreply.github.com>
2020-06-23 11:27:31 +0530

    looks better now @sachinyadav3496

--- first_tkinter.py
+++ first_tkinter.py
@@ -3,6 +3,18 @@ import tkinter as tk
 
 root = tk.Tk()
 
+fg = '#eeeeee'
+bg = '#333333'
+def change_color():
+        global fg
+        if fg == '#eeeeee':
+                fg = '#333333'
+                bg = '#eeeeee'
+                label.config(fg=fg, bg=bg)
+        else:
+                fg = '#eeeeee'
+                bg='#333333'
+                label.config(fg=fg, bg=bg)
 root.title("Hello World Application")
 
 label = tk.Label(text="Hello World", fg='#eeeeee', bg='#333333', height=10, width=30,
@@ -10,5 +22,6 @@ label = tk.Label(text="Hello World", fg='#eeeeee', bg='#333333', height=10, widt
 
 label.pack(fill=tk.BOTH, expand=tk.YES)
 
+label.after(1000, change_color)
 
 root.mainloop()
