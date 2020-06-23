bc4472376ca6b0f01f6077cdad8770eee18dbb7c
sachinyadav3496 <sachinyadav3496@gmail.com>
2020-06-23 11:24:34 +0530

    updated firsttkinter.py to look better @sachin3496

--- first_tkinter.py
+++ first_tkinter.py
@@ -3,7 +3,9 @@ import tkinter as tk
 
 root = tk.Tk()
 
-label = tk.Label(text="Hello World", fg='white', bg='black', height=10, width=30,
+root.title("Hello World Application")
+
+label = tk.Label(text="Hello World", fg='#eeeeee', bg='#333333', height=10, width=30,
         font=('Times', 40, 'bold'))
 
 label.pack(fill=tk.BOTH, expand=tk.YES)
